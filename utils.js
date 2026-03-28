54af94087fd698ce10657f6b28b613a48be4efb7
Arek Golen <arek125@gmail.com>
2026-03-28 16:24:09 +0100

    fix

--- utils.js
+++ utils.js
@@ -83,7 +83,7 @@ function formatRawCanFrameData2(frame) {
   const dataArray = []; 
   // Safely read data bytes up to DLC length
   for (let i = 0; i < dlc && i < frame.data.byteLength; i++) {
-    dataArray.push(dataView.getUint8(i));
+    dataArray.push(frame.data.getUint8(i));
   }
   // Use frame timestamp if available, otherwise use current time
   const timestamp = frame.timestamp_us || Date.now() * 1000;
